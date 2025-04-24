f5868be6c715f39373b7d5bf11c91d29e3f0af08
wonjun ka <ffkaka77@gmail.com>
2025-04-24 10:40:03 +0900

    UPD: update theme / enable jdtls

--- lua/ffkaka/plugins/nvim-lspconfig.lua
+++ lua/ffkaka/plugins/nvim-lspconfig.lua
@@ -171,7 +171,9 @@ return { -- LSP Configuration & Plugins
 					},
 				},
 			},
-			-- jdtls = {},
+			jdtls = {
+				ft = { "java" },
+			},
 			cmake = {},
 			-- rust_analyzer = {},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
